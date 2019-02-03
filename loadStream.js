49bda6f938864a492be0e07f206465ee4a444716
Sebastian Esp Nielsen <sebastianesp@hotmail.com>
2019-02-03 19:04:25 +0100

    loading from input

--- loadStream.js
+++ loadStream.js
@@ -6,7 +6,7 @@ function loadStream(uri){
         hls.attachMedia(video);
         hls.on(Hls.Events.MEDIA_ATTACHED, function () {
             console.log("video and hls.js are now bound together !");
-            hls.loadSource("https://vod-metro.twitch.tv/88ffecacf7acec4a9555_northernlion_32450291536_1096768652/chunked/index-dvr.m3u8");
+            hls.loadSource(uri);
             hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
             console.log("manifest loaded, found " + data.levels.length + " quality level");
             });
